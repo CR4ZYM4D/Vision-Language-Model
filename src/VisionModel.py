29cd46729b460f9f97fdaf5eaf7cbdc68a7a4105
Madhav <madhav@Buzos-stronghold.laptop.net>
2025-06-04 23:00:33 +0530

    Vision Transformer Embedding Layer

--- src/VisionModel.py
+++ src/VisionModel.py
@@ -2,22 +2,55 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as f 
 
+# class for the embedding layer of the vision transformer model
 
+class VisionEmbedding(nn.Module):
+
+    def __init__(self, vector_dimension, patch_size, num_channels, image_size):
+        super().__init__()
+
+        self.convolutor = nn.Conv2d(
+            in_channels = num_channels,  # 3 channels per image R, G, B
+            out_channels= vector_dimension, 
+            kernel_size= patch_size,
+            stride = patch_size
+            )
+        
+        self.num_patches = (image_size // patch_size) ** 2
+
+        self.positionalEmbedding = nn.Embedding(self.num_patches, vector_dimension)
+
+        self.register_buffer("positions", torch.arange(self.num_patches).expand((1, -1)), persistent= False)
+
+    def forward(self, image_tensors):
+
+        # image tensors are of shape [ batch_size x num_channels x height x width ] 
+
+        # converting them to tensors of shape [ batch_size x vector_dimension x (height / patch_size) x (width / patch_size)]
+        # using the CNN
+
+        image_embeddings = self.convolutor(image_tensors) # [ batch_size x vector_dimension x (height / patch_size) x (width / patch_size) ]
+
+        image_embeddings = image_embeddings.flatten(2) # [ batch_size x vector_dimension x num_patches ]
+
+        image_embeddings = image_embeddings.transpose(1, 2) # [ batch_size x num_patches x vector_dimension ]
+
+        return image_embeddings + self.positionalEmbedding(self.positions)
 
 # class to make the vision Transformer Model
 
 class VisionTransformer(nn.Module):
 
-    def __init__(self, vector_dimension, normalization_constant):
+    def __init__(self, vector_dimension, normalization_constant, patch_size, num_channels, image_size):
         super().__init__()
 
-        self.embeddingLayer
+        self.embeddingLayer = VisionEmbedding(vector_dimension, patch_size, num_channels, image_size)
         self.encoderLayer
         self.postNormalizationLayer = nn.LayerNorm(vector_dimension, eps = normalization_constant)
 
-    def forward(self, image_tensor):
+    def forward(self, image_tensors):
 
-        hidden_state = self.embeddingLayer(image_tensor)
+        hidden_state = self.embeddingLayer(image_tensors)
 
         hidden_state = self.encoderLayer(hidden_state)
 
